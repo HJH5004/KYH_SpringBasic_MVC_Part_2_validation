5036e75ef4ebd4b4ea0c0d052a247bd8d09ee96b
Curry5004 <yunjin5004@naver.com>
2025-03-10 07:48:16 +0900

    * 25. 03. 06
    Valiaidation 로직 분리 v2 어노테이션을 사용한 벨리데이션 로직 진행
    
    BeanValidation 검증기 테스트 코드 작성

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,13 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.addValidators(itemValidator);
+
+    }
+
 
     @GetMapping
     public String items(Model model) {
@@ -192,7 +201,7 @@ public class ValidationItemControllerV2 {
 
 
     //벨리데이션 로직을 validator로 추출
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName = {}", bindingResult.getObjectName());
@@ -200,6 +209,23 @@ public class ValidationItemControllerV2 {
 
         //벨리데이터 다 추출됨
         itemValidator.validate(item, bindingResult);
+
+        //검증 실패하면 바로 return
+        if(bindingResult.hasErrors()){
+            log.error("errors: {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+
+    //Validatior 삽입을 통한 벨리데이션 어노테이션 삽입
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         //검증 실패하면 바로 return
         if(bindingResult.hasErrors()){
